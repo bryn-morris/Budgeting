a2771cf4e30d0731797d0cab5ab6cb303394340e
Brynfor Morris <brynfor.morris@gmail.com>
2026-02-28 17:16:33 -0600

    removed npm from git tracking

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// Entry point. Everything you want in GAS must be reachable from here.
+// import call functions in to be called 
+// import { syncToMcr } from "./syncToMcr.js";
+// import { syncFromMcr } from "./syncFromMcr.js";
+// import { cleanup } from "./cleanup.js";
+
+// export function syncAll() {
+//   syncToMcr();
+//   syncFromMcr();
+//   cleanup();
+// }
